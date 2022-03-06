110e5edcb81149fd795d213b51c7b4435479b3a3
LuiggiAntonni <luiggiantonni0129@gmail.com>
2022-03-06 11:24:13 -0300

    Add body-parser

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express =  require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 
 const rotaMain = require('./routes/main');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.use('/main', rotaMain);
 
 app.use((req, res, next) => {
     const erro = new Error('Não encontrado')
-    erro.status(404);
+    erro.status = 404;
     next(erro);
-})
+});
+
+app.use((error,req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        mensagem: error.message
+    })
+});
 
 module.exports = app;
