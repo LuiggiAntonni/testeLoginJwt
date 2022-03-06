83ab86050559384c0fd02528baea4dbe023093c9
LuiggiAntonni <luiggiantonni0129@gmail.com>
2022-03-06 11:01:35 -0300

    Adicionando morgan e erro 404

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express =  require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaMain = require('./routes/main');
 
+app.use(morgan('dev'));
+
 app.use('/main', rotaMain);
+
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado')
+    erro.status(404);
+    next(erro);
+})
 
 module.exports = app;
